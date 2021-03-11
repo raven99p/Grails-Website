3d729cf329056a44435bb195f49f6fdc8deb7a96
raven <paulinhoellul@gmail.com>
2021-03-11 13:22:14 +0200

    Fixed AFM from integer to varchar

--- grails-app/services/helloworld/SimpleService.groovy
+++ grails-app/services/helloworld/SimpleService.groovy
@@ -39,7 +39,6 @@ class SimpleService {
 
     def updateEmp(params) {
         def sql = new Sql(dataSource)
-        def afm = params.afm.toInteger()
         def id = params.id.toInteger()
         def dob = Date.valueOf(params.dob)
         def d_id = params.dept_id.toInteger()
@@ -51,21 +50,20 @@ class SimpleService {
                                             dob=:dob, 
                                             dept_id=:d_id  
                                         where id=:id
-                                        ''',[fName:params.first_name, lName:params.last_name, afm:afm, id:id, dob:dob, d_id:d_id])
+                                        ''',[fName:params.first_name, lName:params.last_name, afm:params.afm, id:id, dob:dob, d_id:d_id])
         sql.close()
         return res
     }
 
     def createEmp(params) {
         def sql = new Sql(dataSource)
-        def afm = params.afm.toInteger()
         def dob = Date.valueOf(params.dob)
         def dept_id = params.dept_id.toInteger()
         def resultRows = sql.execute('''
                                         insert into employee 
                                             (first_name,last_name,afm,dob,dept_id) 
                                             values (?,?,?,?,?)
-                                        ''',[params.first_name,params.last_name,afm,dob,dept_id])
+                                        ''',[params.first_name,params.last_name,params.afm,dob,dept_id])
 
         sql.close()
         return resultRows
