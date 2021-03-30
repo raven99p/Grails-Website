32399460b43d2af604420cf89e5bb4c53778845f
pellul <paulinhoellul@gmail.com>
2021-03-30 16:02:23 +0300

    Added cookie creation and deletion

--- grails-app/controllers/Authentication/AuthenticationResponderController.groovy
+++ grails-app/controllers/Authentication/AuthenticationResponderController.groovy
@@ -1,23 +1,37 @@
 package Authentication
 
+import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.SignatureGenerationException;
 import com.auth0.jwt.impl.ClaimsHolder;
 import com.auth0.jwt.impl.PayloadSerializer;
-import com.auth0.jwt.impl.PublicClaims;
+import com.auth0.jwt.impl.PublicClaims
+
+import javax.servlet.http.Cookie;
 
 class AuthenticationResponderController {
     def authService
-
+    static responseFormats = ['json']
     def verify() {
         def userFound = authService.getUserInformation(request.getJSON())
         if (userFound) {
             def nickname = request.getJSON().username.split('@')
+            try {
+                Algorithm algorithm = Algorithm.HMAC256('F90C6860-70C1-4B33-BE08-30E160B54CB6');
+                String token = JWT.create()
+                        .withIssuer("auth0")
+                        .sign(algorithm);
+                Cookie cookie = new Cookie("authentication", token)
+                cookie.maxAge = 3600
+                cookie.httpOnly = true
+                cookie.setPath("/")
+                response.addCookie(cookie)
 
+                respond(status: 200, formats: responseFormats)
+            } catch (JWTCreationException exception) {
+            }
 
-            response.setCookie('authentication', nickname[0])
-            respond (status: 200, cookie: request.getCookie('authentication') )
             return true
         } else {
             flash.message = "Τα στοιχεία που εισάγατε είναι λάθος"
@@ -26,9 +40,14 @@ class AuthenticationResponderController {
         }
 
     }
+
     def logout() {
-        response.deleteCookie('authentication')
-        respond (status: 200, cookie: request.getCookie('authentication'))
+        Cookie cookie = new Cookie("authentication", null); // Not necessary, but saves bandwidth.
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(0); // Don't set to -1 or it will become a session cookie!
+        response.addCookie(cookie);
+        respond(status: 200)
     }
 
 
